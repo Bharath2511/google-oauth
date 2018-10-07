c0b131dea24468ebc2cf7fea9a051f2ca62f68a1
V S Bharath <bharathcse5t6@gmail.com>
2018-10-07 16:57:45 +0530

    added login logout routes

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require('express')
-
+const authRoutes = require('./routes/auth-routes')
 const app = express()
 
 //set up view engine
 app.set('view engine','ejs')
 
-app.listen(2500,()=>{
-    console.log('2500')
+// set up routes
+app.use('/auth',authRoutes)
+
+app.listen(2700,()=>{
+    console.log('2700')
 })
 
 //create home page
