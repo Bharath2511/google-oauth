0bc3a927d206dc052f474db095814876aa17e07e
V S Bharath <bharathcse5t6@gmail.com>
2018-10-08 19:59:50 +0530

    customising login logout views

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -2,7 +2,7 @@ const router = require('express').Router()
 const passport = require('passport')
 //auth login
 router.get('/login',(req,res)=>{
-    res.render('login')
+    res.render('login',{user:req.user})
 })
 
 //auth logout
