6e38d4d8ebfe9715a00b7f62076dec0bc96195c3
V S Bharath <bharathcse5t6@gmail.com>
2018-10-08 11:06:48 +0530

    connected to mlab

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const express = require('express')
 const authRoutes = require('./routes/auth-routes')
-const app = express()
 const passportSetup = require('./config/passport-setup')
+const mongoose = require('mongoose')
+const keys = require('./config/keys')
+
+const app = express()
+
+//connect to mongodb
+mongoose.connect(keys.mongoDB.mongoURI,()=>{
+    console.log('connected to mongodb')
+})
 
 //set up view engine
 app.set('view engine','ejs')
