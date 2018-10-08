7b9bbc1b72c324aa04c2131056023511b52ee95f
V S Bharath <bharathcse5t6@gmail.com>
2018-10-08 19:34:20 +0530

    customizing the profile page

--- routes/profile-routes.js
+++ routes/profile-routes.js
@@ -10,7 +10,7 @@ const authCheck = (req,res,next) => {
     }
 }
 router.get('/',authCheck,(req,res)=>{
-    res.send('profile page of :',req.user.username)
+    res.render('profile',{user:req.user})
 })
 
 module.exports = router
