3bf11d1946b2f39b4cbba49739dc869e163e8c04
V S Bharath <bharathcse5t6@gmail.com>
2018-10-08 16:08:35 +0530

    made database redundant free

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -11,12 +11,24 @@ passport.use(new googleStrategy({
     clientSecret : keys.google.clientSecret
 },(accessToken,refreshToken,profile,done)=>{
     //passport callback function
-console.log(profile)
-new User({
-    username:profile.displayName,
-    googleId : profile.id
+//console.log(profile)
+//check if user already exists
+User.findOne({
+    googleId:profile.id
+}).then((currentUser)=>{
+  if(currentUser) {
+      //already have user
+      console.log('user is:',currentUser)
+  } else {
+      //create user
+      new User({
+        googleId: profile.id,
+        username: profile.displayName
 }).save().then((newUser)=>{
-    console.log('new user created: '+newUser)
+console.log('new user created: '+ newUser)
 })
+  }
+})
+
 })
 )
